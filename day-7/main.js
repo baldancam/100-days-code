6b2f9324624087b4dc23ab65374ad9fd00ab2dfb
matheus <baldanam@gmail.com>
2024-02-23 17:03:38 -0300

    day-7

---
+++ day-7/main.js
@@ -0,0 +1,8 @@
+function adicionarConteudo(idConteudo) {
+  // Obtém o texto digitado no input
+  var texto = document.getElementById("input" + idConteudo.slice(8)).value;
+  // Adiciona o texto ao conteúdo da coluna correspondente
+  document.getElementById(idConteudo).innerHTML += "<p>" + texto + "</p>";
+  // Limpa o input
+  document.getElementById("input" + idConteudo.slice(8)).value = "";
+}
