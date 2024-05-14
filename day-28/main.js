ffe219c02e91248742528ecd65d0450bd2d06e37
baldancam <baldancama@gmail.com>
2024-05-13 22:47:38 -0300

    day 28

--- day-28/main.js
+++ day-28/main.js
@@ -1,177 +1,33 @@
-// Carrega todos os elementes contidos dentro desse bloco ao carregar a página
-document.addEventListener("DOMContentLoaded", () => {
+function lancarDados() {
+    const selectElement = document.getElementById("faces");
+    const valorSelecionado = parseInt(selectElement.value);
+    const numerosorteado = document.getElementById("numerosorteado");
 
-    const cards = [
-        {
-            name: 'img-1',
-            img: 'img/img-1.png',
-        },
-        {
-            name: 'img-2',
-            img: 'img/img-2.jpeg',
-        },
-        {
-            name: 'img-3',
-            img: 'img/img-3.jpeg',
-        },
-        {
-            name: 'img-4',
-            img: 'img/img-4.jpeg',
-        },
-        {
-            name: 'img-5',
-            img: 'img/img-5.jpeg',
-        },
-        {
-            name: 'img-6',
-            img: 'img/img-6.jpeg',
-        },
-        {
-            name: 'img-7',
-            img: 'img/img-7.jpeg',
-        },
-        {
-            name: 'img-8',
-            img: 'img/img-8.jpeg',
-        },
-        {
-            name: 'img-9',
-            img: 'img/img-9.jpeg',
-        },
-        {
-            name: 'img-10',
-            img: 'img/img-10.jpeg',
-        },
-        {
-            name: 'img-11',
-            img: 'img/img-11.jpeg',
-        },
-        {
-            name: 'img-12',
-            img: 'img/img-12.jpeg',
-        },{
-            name: 'img-1',
-            img: 'img/img-1.png',
-        },
-        {
-            name: 'img-2',
-            img: 'img/img-2.jpeg',
-        },
-        {
-            name: 'img-3',
-            img: 'img/img-3.jpeg',
-        },
-        {
-            name: 'img-4',
-            img: 'img/img-4.jpeg',
-        },
-        {
-            name: 'img-5',
-            img: 'img/img-5.jpeg',
-        },
-        {
-            name: 'img-6',
-            img: 'img/img-6.jpeg',
-        },
-        {
-            name: 'img-7',
-            img: 'img/img-7.jpeg',
-        },
-        {
-            name: 'img-8',
-            img: 'img/img-8.jpeg',
-        },
-        {
-            name: 'img-9',
-            img: 'img/img-9.jpeg',
-        },
-        {
-            name: 'img-10',
-            img: 'img/img-10.jpeg',
-        },
-        {
-            name: 'img-11',
-            img: 'img/img-11.jpeg',
-        },
-        {
-            name: 'img-12',
-            img: 'img/img-12.jpeg',
-        }
-    ]
-
-    // embaralhas todas as cartas
-    cards.sort(() => 0.5 - Math.random());
-
-    // carregar elementos html no script
-    const board = document.querySelector('.board');
-    const resultView = document.querySelector('#result');
-
-    let cardsChosen = [] //cartas escolhidas
-    let cardsChosenId = [] //id das cartas escolhidas
-    let cardsChosenWon = [] //cartas combinadas
-    let cardsWon = [] //cartas combinadas
-
-    //checagem de combinações
-    function checkForMatch() {
-        const cards = document.querySelectorAll('img');
-        const optionOneId = cardsChosenId[0];
-        const optionTwoId = cardsChosenId[1];
-
-        //verificar clique na mesma imagem
-        if(optionOneId == optionTwoId) {
-            cards[optionOneId].setAttribute('src', 'img/img-board.jpeg');
-            cards[optionTwoId].setAttribute('src', 'img/img-board.jpeg');
-            alert('Você clicou na mesma imagem');
+    // Verifica se algum número foi selecionado
+    if (valorSelecionado > 0) {
+        const array = []; // array para armazenar o número de lados selecionado
+        for (let i = 1; i <= valorSelecionado; i++) {
+            array.push(i); // adiciona os números dentro do array
         }
 
-        //verificar combinação se click em imagens diferentes
-        else if(cardsChosen[0] === cardsChosen[1]) {
-            cards[optionOneId].setAttribute('src', 'img/img-check.jpeg');
-            cards[optionTwoId].setAttribute('src', 'img/img-check.jpeg');
-            cards[optionOneId].removeEventListener('click', flipCard);
-            cards[optionTwoId].removeEventListener('click', flipCard);
-            cardsWon.push(cardsChosen);
-            alert('Par encontrado!');
-        } else {
-            cards[optionOneId].setAttribute('src', 'img/img-board.jpeg');
-            cards[optionTwoId].setAttribute('src', 'img/img-board.jpeg');
-            alert('Você errou! Tente novamente!');
-        }
+        const interval = setInterval(() => {
+            const numeroAleatorio = selecionarNumeroAleatorio(array);// armazena o número aleatorio gerado
+            numerosorteado.innerText = numeroAleatorio;// exibe o numero sorteado no <span>
+        }, 100); // Intervalo de 200 milissegundos
 
-        cardsChosen = [];
-        cardsChosenId = [];
-    
-        // mostar placar
-        resultView.textContent = 'Pares encontrados: ' + cardsWon.length;
-        if(cardsWon.length === cards.length / 2) {
-            resultView.textContent = 'Parabéns, você encontrar todos os pares!';
-        }
+        setTimeout(() => {
+            clearInterval(interval); // Para a animação após um tempo
+            const numeroFinal = selecionarNumeroAleatorio(array);
+            numerosorteado.innerText = numeroFinal;
+        }, 2000); // Tempo total de 2 segundos para a animação
+
+    } else {
+        alert('Por favor, selecione o número de lados.');
     }
 
-
-    //criar o quadro de castas
-    function createBoard() {
-        for(let i = 0; i < cards.length; i++){
-            const card = document.createElement('img');
-            card.setAttribute('src', 'img/img-board.jpeg');
-            card.setAttribute('data-id', i);
-            card.addEventListener('click', flipCard);
-            board.appendChild(card);
-        }
+     // função gera um número aleatorio
+    function selecionarNumeroAleatorio(array) {
+        const indiceAleatorio = Math.floor(Math.random() * array.length);
+        return array[indiceAleatorio];
     }
-
-    function flipCard(){
-        let cardId = this.getAttribute('data-id');
-        cardsChosen.push(cards[cardId].name);
-        cardsChosenId.push(cardId);
-        this.setAttribute('src', cards[cardId].img);
-
-        if(cardsChosen.length === 2) {
-            setTimeout(checkForMatch, 500);
-        }
-    }
-    
-    
-createBoard();
-    
-});
+}
